bb89c712919f4162ecf185ba90b847eb4aca570f
Vijay Deepak <vijaydeepak7195@outlook.com>
2017-10-30 16:47:56 +0530

    gettodos function changed

--- src/App.js
+++ src/App.js
@@ -16,20 +16,26 @@ class App extends React.Component {
     }
   }
 
+  // getToDos() {
+  //   $.ajax({
+  //     url: 'https://jsonplaceholder.typicode.com/todos',
+  //     dataType: 'json',
+  //     cache: false,
+  //     success: function(data) {
+  //       this.setState({todos: data}, function() {
+  //         console.log(this.state);
+  //       });
+  //     }.bind(this),
+  //     error: function(xhx, status, err) {
+  //       console.log(err);
+  //     }
+  //   })
+  // }
+
   getToDos() {
-    $.ajax({
-      url: 'https://jsonplaceholder.typicode.com/todos',
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({todos: data}, function() {
-          console.log(this.state);
-        });
-      }.bind(this),
-      error: function(xhx, status, err) {
-        console.log(err);
-      }
-    })
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then( response => { return response.json() })
+    .then( (data) => this.setState({todos: data}) );
   }
 
   getProjects() {
